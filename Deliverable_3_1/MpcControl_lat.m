780a002712dbc82d00e307df4b47d492719d0707
SaraHeip <sara.heidaripour@epfl.ch>
2025-01-09 15:11:53 +0330

    Create MpcControl_lat.m

---
+++ Deliverable_3_1/MpcControl_lat.m
@@ -0,0 +1,126 @@
+
+classdef MpcControl_lat < MpcControlBase
+    
+    methods
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   x0           - initial state (estimate)
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   u0           - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            load('terminal_set.mat', 'Ff', 'ff');
+
+            N_segs = ceil(mpc.H/mpc.Ts); % Horizon steps
+            N = N_segs + 1;              % Last index in 1-based Matlab indexing
+
+            [nx, nu] = size(mpc.B);
+
+            % Constraints of the system
+            F = [1, 0; -1, 0; 0, 1; 0, -1];
+            f = [3.5; 0.5; 0.0873; 0.0873];
+            M = [1; -1];
+            m = [0.5236; 0.5236];
+
+            % Targets
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+
+            % Initial states
+            x0 = sdpvar(nx, 1);
+            x0other = sdpvar(nx, 1); % (Ignore this, not used)
+
+            % Input to apply to the system
+            u0 = sdpvar(nu, 1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D
+            %       are the DISCRETE-TIME MODEL of your system
+            %       You can find the linearization steady-state
+            %       in mpc.xs and mpc.us.
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+
+            A = mpc.A;
+            B = mpc.B;
+
+            Q = 50 * eye(nx);
+            R = 1 * eye(nu);
+            [K, Qf, ~] = dlqr(A, B, Q, R);
+            K = -K;
+          
+            % Define optimization variables
+            x = sdpvar(nx, N, 'full');
+            u = sdpvar(nu, N-1, 'full');
+
+            obj = 0;
+            con = x(:, 1) == x0;
+
+            for i = 1:N-1
+                con = [con, x(:, i+1) == A*x(:, i) + B*u(:, i)];
+                con = [con, F * (x(:, i) + mpc.xs) <= f];
+                con = [con, M * (u(:, i) + mpc.us) <= m];
+                obj = obj + (x(:,i) - x_ref)' * Q * (x(:, i) - x_ref) + (u(:, i) - u_ref)' * R * (u(:, i) - u_ref);
+            end
+            con = [con, Ff * (x(:, i) - x_ref) <= ff];
+            obj = obj + (x(:,N) - x_ref)' * Qf * (x(:, N) - x_ref);
+
+
+            % Replace this line and set u0 to be the input that you
+            % want applied to the system. Note that u0 is applied directly
+            % to the nonlinear system. You need to take care of any 
+            % offsets resulting from the linearization.
+            % If you want to use the delta formulation make sure to
+            % substract mpc.xs/mpc.us accordingly.
+            con = con + ( u0 == (u(:, 1) + mpc.us) );
+
+            % Pass here YALMIP sdpvars which you want to debug. You can
+            % then access them when calling your mpc controller like
+            % [u, X, U] = mpc_lat.get_u(x0, ref);
+            % with debugVars = {X_var, U_var};
+            debugVars = {};
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {x0, x_ref, u_ref, x0other}, {u0, debugVars{:}});
+        end
+        
+        % Computes the steady state target which is passed to the
+        % controller
+        function [xs_ref, us_ref] = compute_steady_state_target(mpc, ref)
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            % OUTPUTS
+            %   xs_ref, us_ref - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            % Steady-state system
+            A = mpc.A;
+            nx = size(A, 1);
+            B = mpc.B;
+
+            % Linearization steady-state
+            xs = mpc.xs;
+            us = mpc.us;
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % all in delta form
+            xs_ref = [ref; 0] - xs;
+            us_ref = pinv(B) * (eye(nx) - A) * xs_ref;
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        end
+    end
+end
