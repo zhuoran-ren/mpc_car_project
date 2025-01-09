815b67173fa93caf9520e6d2bec065a883b4162d
SaraHeip <sara.heidaripour@epfl.ch>
2025-01-10 00:36:39 +0330

    Add files via upload

---
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -0,0 +1,41 @@
+addpath("common\")
+addpath("Deliverable_6_1\")
+%%
+Ts = 1/10;
+car = Car(Ts);
+H = 10;
+mpc = NmpcControl(car, H);
+
+%% Check in the open loop
+x0 = [0 0 0 80/3.6];
+ref = [3 100/3.6];
+u = mpc.get_u(x0, ref);
+
+%% Check in the close loop
+params = {};
+params.Tf = 15;
+params.myCar.model = car;
+params.myCar.x0 = [0 0 0 80/3.6];
+params.myCar.u = @mpc.get_u;
+ref1 = [0 80/3.6]';
+ref2 = [0 120/3.6]';
+params.myCar.ref = car.ref_step(ref1, ref2, 2);
+
+result = simulate(params);
+visualization(car, result);
+%%
+x0_ego = [0 0 0 80/3.6]';
+x0_other = [20 0 0 80/3.6]';
+ref1 = [0 80/3.6]';
+ref2 = [0 100/3.6]';
+params = {};
+params.Tf = 15;
+params.myCar.model = car;
+params.myCar.x0 = x0_ego;
+params.myCar.u = @mpc.get_u;
+params.myCar.ref = car.ref_step(ref1, ref2, 1);
+params.otherCar.model = car;
+params.otherCar.x0 = x0_other;
+params.otherCar.u = car.u_const(80/3.6);
+result = simulate(params);
+visualization(car, result);
