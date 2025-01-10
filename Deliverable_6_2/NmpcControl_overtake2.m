e981fab6f61a1857b136af034dd75174dac71b0b
SaraHeip <sara.heidaripour@epfl.ch>
2025-01-11 01:14:39 +0330

    Update and rename new2.m to NmpcControl_overtake2.m

--- Deliverable_6_2/NmpcControl_overtake2.m
+++ Deliverable_6_2/NmpcControl_overtake2.m
@@ -1,4 +1,4 @@
-classdef NmpcControl < handle
+classdef NmpcControl_overtake < handle
 
     properties
         % The NMPC problem
@@ -29,7 +29,7 @@ classdef NmpcControl < handle
     end
 
     methods
-        function obj = NmpcControl(car, H)
+        function obj = NmpcControl_overtake(car, H)
 
             import casadi.*
 
@@ -62,10 +62,10 @@ classdef NmpcControl < handle
 
 	        
             for k =1:N
-                cost = cost + 7000*obj.U(1,k)^2+ 11*obj.U(2,k)^2;
-                cost= cost + 3*obj.X(3,k)^2;
-                cost = cost + 0.5*(obj.X(2,k)-obj.ref(1))*(obj.X(2,k)-obj.ref(1))';
-                cost = cost + 3*(obj.X(4,k)-obj.ref(2))*(obj.X(4,k)-obj.ref(2))'; 
+                cost = cost + 7000*obj.U(1,k)^2+ 11*obj.U(2,k)^2 + ...
+                            + cost + 3*obj.X(3,k)^2 + ...
+                            + 0.5*(obj.X(2,k)-obj.ref(1))^2 + ...
+                            + 3*(obj.X(4,k)-obj.ref(2))^2; 
                 
             end
             
